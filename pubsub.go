76735b903d376108411ea3b6eecd48cbf4af9b1f
pedroaston <pedroaston@gmail.com>
2021-06-14 15:32:29 +0000

    fixing filtertable bug for non-dual peer connections

--- pubsub.go
+++ pubsub.go
@@ -301,15 +301,11 @@ func (ps *PubSub) Subscribe(ctx context.Context, sub *pb.Subscription) (*pb.Ack,
 		aux = append(aux, addr)
 	}
 
-	// DEBUG >> Catch the guy
-	if ps.currentFilterTable == nil {
-		fmt.Println("Puum!")
-	} else if sub == nil {
-		fmt.Println("Boom!")
-	} else if sub.PeerID == "" {
-		fmt.Println("Badum!")
-	} else if ps.currentFilterTable.routes[sub.PeerID] == nil {
-		fmt.Println("Plock!")
+	if ps.currentFilterTable.routes[sub.PeerID] == nil {
+		ps.tablesLock.Lock()
+		ps.currentFilterTable.routes[sub.PeerID] = NewRouteStats()
+		ps.nextFilterTable.routes[sub.PeerID] = NewRouteStats()
+		ps.tablesLock.Unlock()
 	}
 
 	ps.tablesLock.RLock()
